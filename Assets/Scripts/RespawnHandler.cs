e265befa59163a5773619e233cf371bd0e04cebf
SirSalt <stijn.bellemans@gmail.com>
2025-04-08 12:46:47 +0200

    RespawnHandler

---
+++ Assets/Scripts/RespawnHandler.cs
@@ -0,0 +1,31 @@
+using System.Drawing;
+using UnityEngine;
+
+public class RespawnHandler : MonoBehaviour
+{
+
+
+    private Rectangle _bounds = new Rectangle(0,0,100,50);
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HitBoundary()
+    {
+
+    }
+  
+
+
+
+
+}
