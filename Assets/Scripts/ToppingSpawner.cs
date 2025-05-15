6f67b1f3b9bf6db28653c272e4edc1a9f86c9410
BananaMoray <freyakropsch@gmail.com>
2025-05-15 14:57:11 +0200

    RespawnHandler Lobotomy

---
+++ Assets/Scripts/ToppingSpawner.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ToppingSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _players;
+    [SerializeField] 
+    private QueDelay _delay;
+    [SerializeField] 
+    private bool _start;
+
+
+    [SerializeField]
+    private GameObject _toppingPrefab;
+    [SerializeField] 
+    private int _maxWeight = 15;
+    [SerializeField]
+    private float _radius = 15f;
+    [SerializeField]
+    private float _spawnDelay = 0;
+    [SerializeField]
+    private float _toppingSpawnTimer;
+    [SerializeField]
+    private float _maxTime = 3f;
+    [SerializeField]
+    private int _height = 15;
+
+    private void Update()
+    {
+        _start = _delay._startGame;
+
+        if (_start)
+        {
+            if (_players.Length == 0)
+            {
+                _players = GameObject.FindGameObjectsWithTag("Player");
+            }
+
+            _spawnDelay -= Time.deltaTime;
+            if (_spawnDelay <= 0)
+            {
+                SpawnAtRandomCirclePosition();
+            }
+        }
+    }
+
+    void SpawnAtRandomCirclePosition()
+    {
+        _toppingSpawnTimer -= Time.deltaTime;
+
+        float angle = Random.Range(0f, Mathf.PI * 2f);
+        Vector3 spawnPosition = new Vector3(Mathf.Cos(angle) * _radius, _height, Mathf.Sin(angle) * _radius);
+
+        if (_toppingSpawnTimer <= 0)
+        {
+            int selectedPlayerIndex = GetPlayerByWeight();
+
+            GameObject topping = Instantiate(_toppingPrefab, spawnPosition, Quaternion.identity);
+            ToppingHandler toppingHandler = topping.GetComponent<ToppingHandler>();
+
+            if (toppingHandler != null)
+            {
+                toppingHandler.PlayerIndex = selectedPlayerIndex;
+            }
+
+            _toppingSpawnTimer = _maxTime;
+        }
+
+
+    }
+
+    int GetPlayerByWeight()
+    {
+        //we are making a list for the topping count per player
+        int[] toppingCounts = new int[_players.Count()];
+
+        //we find ALL toppings and slapp them into the topping counter
+        //no i dont know why FindObjectByType isnt working
+        foreach (ToppingHandler topping in FindObjectsOfType<ToppingHandler>())
+        {
+            if (topping.PlayerIndex >= 0 && topping.PlayerIndex < _players.Count())
+                toppingCounts[topping.PlayerIndex]++;
+        }
+
+        List<int> weightList = new List<int>();
+
+        for (int i = 0; i < _players.Count(); i++)
+        {
+            // we calculate how many toppings the player has
+            // a player with 0 toppings has a weight of _maxWeight - 0 to have a topping spawn
+            //a player with 5 toppings has a weight of _maxWeight - 5
+            //we add a math max so that players with more than _maxWeight toppings dont have a weight of 0 but 1 (there should always be a chance to get toppings)
+            int weight = Mathf.Max(1, _maxWeight - toppingCounts[i]);
+
+
+            //this one is pretty stupid but shoudl still work for now
+            //if a player has a weight of 5, they get added 5 timnes to the weightList
+            //the weightList will define what topping spawns next
+            for (int j = 0; j < weight; j++)
+            {
+                weightList.Add(i);
+            }
+        }
+
+        //imagine it like this: weightList[0,0,0,0,0,0,1,1,2,2,2,2,2,3,3]
+        return weightList[Random.Range(0, weightList.Count)];
+    }
+}
